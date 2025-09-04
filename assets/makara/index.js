7847b912b69cb319c91a50bcbbc07fe91bcddd2f
CMMakara <makaragamer91@gmai.com>
2025-09-04 14:13:38 +0700

    update

--- assets/makara/index.js
+++ assets/makara/index.js
@@ -6,3 +6,25 @@ function changeQty(amount, inputId) {
   if (current < 1) current = 1;
   input.value = current;
 }
+
+// Toast handler
+const toastTrigger = document.getElementById('saveBtn');
+const toastLiveExample = document.getElementById('save');
+
+if (toastTrigger) {
+  toastTrigger.addEventListener('click', () => {
+    const toastBootstrap = bootstrap.Toast.getOrCreateInstance(
+      toastLiveExample,
+      { delay: 3000 }
+    );
+    toastBootstrap.show();
+  });
+}
+//delete
+const deleteBtn = document.getElementById('deleteBtn');
+    const deleteToastEl = document.getElementById('deleteToast');
+    const deleteToast = new bootstrap.Toast(deleteToastEl);
+
+    deleteBtn.addEventListener('click', () => {
+      deleteToast.show();
+    });
