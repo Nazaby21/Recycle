9116316dbc6792b3e4c21b11d56f9db93f38ca56
Measrithy_Nazaby <nazabymeas7@gmail.com>
2025-09-11 14:08:24 +0700

    push

--- assets/js/function.js
+++ assets/js/function.js
@@ -5,3 +5,16 @@ links.forEach(link => {
     e.currentTarget.classList.add('active');
   });
 });
+
+// animation
+var animation = document.getElementById('text-animation');
+
+var typewriter = new Typewriter(animation, {
+    loop: true
+});
+
+typewriter
+  .typeString('រួមគ្នាថែរក្សាបរិស្ថាន')
+  .pauseFor(2500)
+  .start();
+
