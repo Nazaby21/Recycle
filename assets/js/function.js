3a578dfffe5469aff24c33265737041a3739bb75
Measrithy_Nazaby <nazabymeas7@gmail.com>
2025-09-23 00:47:58 +0700

    push

--- assets/js/function.js
+++ assets/js/function.js
@@ -1,23 +1,19 @@
-const links = document.querySelectorAll('.nav-link.nav-hover');
-links.forEach(link => {
-  link.addEventListener('click', e => {
-    links.forEach(l => l.classList.remove('active'));
-    e.currentTarget.classList.add('active');
+const links = document.querySelectorAll(".nav-link.nav-hover");
+links.forEach((link) => {
+  link.addEventListener("click", (e) => {
+    links.forEach((l) => l.classList.remove("active"));
+    e.currentTarget.classList.add("active");
   });
 });
 
 // animation
-var animation = document.getElementById('text-animation');
+var animation = document.getElementById("text-animation");
 
 var typewriter = new Typewriter(animation, {
-    loop: true
+  loop: true,
 });
 
-typewriter
-  .typeString('រួមគ្នាថែរក្សាបរិស្ថាន')
-  .pauseFor(2500)
-  .start();
-
+typewriter.typeString("រួមគ្នាថែរក្សាបរិស្ថាន").pauseFor(2500).start();
 
 // AOS Animation
 AOS.init({
